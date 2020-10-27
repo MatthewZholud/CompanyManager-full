70fa9a048fdc3e9f4cec2d1d79ec486a5b88afb1
matthewzholud <matthewzholud@gmail.com>
2020-10-28 02:11:10 +0300

    added getall and update functions

--- CompanyManager-tgbot/internal/handlers/company.go
+++ CompanyManager-tgbot/internal/handlers/company.go
@@ -1,16 +1,21 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/MatthewZholud/CompanyManager-full/CompanyManager-tgbot/internal/kafka/producers"
+	"github.com/MatthewZholud/CompanyManager-full/CompanyManager-tgbot/internal/presenter"
 
 	"github.com/MatthewZholud/CompanyManager-full/CompanyManager-tgbot/internal/kafka/consumers"
 
 	"github.com/MatthewZholud/CompanyManager-full/CompanyManager-tgbot/internal/logger"
 )
 const (
+	CompanyGETRequest = "CompanyGETRequest"
 	CompanyGETAllRequest = "CompanyGETAllRequest"
+	CompanyPUTRequest = "CompanyPUTRequest"
+	CompanyGETResponse = "CompanyGETResponse"
 	CompanyGETAllResponse = "CompanyGETAllResponse"
-
+	CompanyPUTResponse = "CompanyPUTResponse"
 )
 
 
@@ -32,4 +37,45 @@ func GetCompanies () []byte {
 	//	logger.Log.Errorf("Can't convert json to employee array: %v", err)
 	//	return
 	//}
+}
+
+func GetCompany(id string) *presenter.Company {
+	var company *presenter.Company
+
+	byteUUID, err := producers.KafkaSend([]byte(id), CompanyGETRequest)
+	if err != nil {
+		logger.Log.Errorf("Error sending message to kafka: %v", err)
+		return nil
+	}
+	msg, err := consumers.KafkaGetStruct(CompanyGETResponse, byteUUID)
+	if err != nil {
+		logger.Log.Errorf("Error sending message to kafka: %v", err)
+		return nil
+	}
+	company, err = JsonToCompany(msg)
+	if err != nil {
+		logger.Log.Errorf("Can't convert json to company struct: %v", err)
+		return nil
+	}
+	return company
+}
+
+func UpdateCompany(company *presenter.Company) string {
+	comp, err := json.Marshal(company)
+	if err != nil {
+		logger.Log.Errorf("Can't prepare company struct for sending to env: %v", err)
+		return ""
+
+	}
+	byteUUID, err := producers.KafkaSend(comp, CompanyPUTRequest)
+	if err != nil {
+		logger.Log.Errorf("Error sending message to env: %v", err)
+		return ""
+	}
+	msg, err := consumers.KafkaGetStruct(CompanyPUTResponse, byteUUID)
+	if err != nil {
+		logger.Log.Errorf("Error sending message to env: %v", err)
+		return ""
+	}
+	return string(msg)
 }
--- CompanyManager-tgbot/internal/server.go
+++ CompanyManager-tgbot/internal/server.go
@@ -1,10 +1,28 @@
 package internal
 
 import (
+	"fmt"
 	"github.com/MatthewZholud/CompanyManager-full/CompanyManager-tgbot/internal/handlers"
 	"github.com/MatthewZholud/CompanyManager-full/CompanyManager-tgbot/internal/logger"
+	"github.com/MatthewZholud/CompanyManager-full/CompanyManager-tgbot/internal/presenter"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"os"
+)
+
+
+var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Company Name","Company Name"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Company Legal form","Company Legal form"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Save","Save"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Break","Break"),
+	),
 )
 
 func StartBot() {
@@ -24,41 +42,111 @@ func StartBot() {
 	if err != nil {
 		logger.Log.Errorf("Check GetUpdatesChan Run for %v", err)
 	}
+	for  {
+		Listen(updates, bot)
+	}
+}
 
+func Listen (updates tgbotapi.UpdatesChannel, bot *tgbotapi.BotAPI) *tgbotapi.Message  {
 	for update := range updates {
+
 		if update.Message == nil {
 			continue
 		}
-
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 		if update.Message.IsCommand() {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			switch update.Message.Command() {
 			case "help":
-				msg.Text = "type /sayhi or /status."
+				msg.Text = "type /getCompanies or /updateCompany."
 			case "getCompanies":
-				responce := handlers.GetCompanies()
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, string(responce))
+				response := handlers.GetCompanies()
+				msg.Text = string(response)
 				bot.Send(msg)
+			case "updateCompany":
+				var oldComp presenter.Company
+				msg.Text = "Please, enter company id:"
+				bot.Send(msg)
+				msg1 := Listen(updates, bot)
+				msg = tgbotapi.NewMessage(msg1.Chat.ID, msg1.Text)
+				compStr := handlers.GetCompany(msg.Text)
+				oldComp.Name = compStr.Name
+				oldComp.ID = compStr.ID
+				oldComp.Legalform = compStr.Legalform
 
-				//resp := ListenId(updates, bot)
+				compStr = ButtonListen(updates, bot, msg, compStr)
 
-				//fmt.Printf("%T", resp)
-				//handlers.GetCompany(resp)
+				if compStr == nil {
+					msg.Text = "Break"
+					bot.Send(msg)
+					continue
+				}
+				if oldComp.ID == compStr.ID && oldComp.Name == compStr.Name && oldComp.Legalform == compStr.Legalform{
+					msg.Text = "You didn't changed anything:"
+					bot.Send(msg)
+					continue
+				}
+
+				msg.Text = fmt.Sprintf("New Company Info:\nCompany ID: %v\nCompany Name: %s\nCompany Legal form: %s\nSelect what parameter do you whant to change?",
+					compStr.ID, compStr.Name, compStr.Legalform)
+				bot.Send(msg)
+				Successful := handlers.UpdateCompany(compStr)
+				if Successful != "Successful update" {
+					logger.Log.Errorf("Updating failed: ")
+				} else {
+					msg.Text = "Successful update"
+					bot.Send(msg)
+					logger.Log.Infof("Successful update")
+				}
+
 
 			default:
 				msg.Text = "I don't know that command"
 			}
-			//bot.Send(msg)
+		} else {
+			return update.Message
 		}
 	}
+	return nil
 }
 
-func Listen (updates tgbotapi.UpdatesChannel, bot *tgbotapi.BotAPI) string  {
+
+func ButtonListen(updates tgbotapi.UpdatesChannel, bot *tgbotapi.BotAPI, msg tgbotapi.MessageConfig, comp *presenter.Company) *presenter.Company {
+	msg.Text = fmt.Sprintf("New Company Info:\nCompany ID: %v\nCompany Name: %s\nCompany Legal form: %s\nSelect what parameter do you whant to change?",
+		comp.ID, comp.Name, comp.Legalform)
+	msg.ReplyMarkup = numericKeyboard
+	bot.Send(msg)
 	for update := range updates {
-		if update.Message == nil {
-			continue
+		msg.ReplyMarkup = nil
+		if update.CallbackQuery != nil{
+			switch update.CallbackQuery.Data {
+			case "Break":
+				comp = nil
+				return comp
+
+			case "Save":
+				return comp
+
+			case "Company Name":
+				msg.Text = "Enter new Company Name:"
+				bot.Send(msg)
+				msg1 := Listen(updates, bot)
+				comp.Name = msg1.Text
+				comp = ButtonListen(updates, bot, msg, comp)
+				return comp
+
+
+			case "Company Legal form":
+				msg.Text = "Enter new Legal form:"
+				bot.Send(msg)
+				msg1 := Listen(updates, bot)
+				comp.Legalform = msg1.Text
+				comp = ButtonListen(updates, bot, msg, comp)
+				return comp
+
+			default:
+				return nil
+			}
 		}
-		return update.Message.Text
 	}
-	return ""
+	return nil
 }
